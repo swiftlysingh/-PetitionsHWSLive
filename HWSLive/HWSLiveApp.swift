4b27333ec60f479312bcbabf2bef561c05657229
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2021-08-02 19:13:36 +0530

    Initial Commit

---
+++ HWSLive/HWSLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSLiveApp.swift
+//  HWSLive
+//
+//  Created by Pushpinder Pal Singh on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct HWSLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
